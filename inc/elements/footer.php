e38864bc8620994990d9b59c6d3684f1147910b5
Ashitosh Yadav <Ashitosh Yadav>
2016-10-10 17:06:21 +0530

    Major Changes

---
+++ inc/elements/footer.php
@@ -0,0 +1,27 @@
+</div>
+	</div>
+</section>
+<footer class="container">
+  <div class="row">
+    <p class="col-sm-4">&copy; 2016 SpaceBaar&nbspEditography</p>
+    <ul class="col-sm-8">
+    	<li class="col-sm-1">
+		<a href="https://twitter.com/ashitoshyadav" target="_blank">
+			<img src="images/twitter.svg" alt="twitter"/>
+		</a>
+	</li>
+  	<li class="col-sm-1">
+		<a href="https://www.facebook.com/yadavashitosh24" target="_blank">
+			<img src="images/facebook.svg" alt="facebook"/>
+		</a>
+	</li>
+      	<li class="col-sm-1">
+		<a href="https://www.instagram.com/yadavashitosh24" target="_blank">
+			<img src="images/instagram.svg" alt="instagram"/>
+		</a>
+	</li>  		
+    </ul>
+  </div>
+</footer>
+</body>
+</html>	
---
+++ inc/php/config.php
@@ -0,0 +1,5 @@
+<?php
+
+//connect to the database
+mysql_connect('mysql.hostinger.in', 'u114179183_ash', '21072410') or die("I couldn't connect to your database, please make sure your info is correct!");
+mysql_select_db('u114179183_space') or die("I couldn't find the database table ($table) make sure it's spelt right!");
