5b6bbd16af65968f62b3957805555cfedd39d0fc
Ashitosh Yadav <Ashitosh Yadav>
2016-11-26 17:38:41 +0530

    Added gift box

--- inc/elements/header.php
+++ inc/elements/header.php
@@ -4,8 +4,20 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="content-language" content="ll-cc">
+    <meta name="description" content="SpaceBaar Editography, A website dedicated to artistic creations of Ashitosh Yadav.">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>SpaceBaar Editography</title>
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-34187384-1', 'auto');
+  ga('send', 'pageview');
+
+  </script>
     <style>
 	body { padding-top: 55px; }
 	</style>
@@ -45,7 +57,6 @@
         <li><a href="portfolio.html">Portfolio</a></li>
         <li><a href="http://blog.spacebaar.com">Blog</a></li>
         <li><a href="aboutus.html">About Us</a></li>
-        <li><a href="contactus.html">Contact Us</a></li>
         <!--<li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
           <ul class="dropdown-menu">
